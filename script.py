9748db19d745688a48a5cb982e776a5ae308a1e3
anat mas <mastryukov1990@gmail.com>
2020-09-19 01:16:18 +0300

    commets added

--- script.py
+++ script.py
@@ -2,31 +2,33 @@ import torch
 import sys
 import json
 from torchvision import transforms
-print('load model...')
-model  = torch.load('resner10111',map_location=torch.device('cpu'))
-transform_val = transforms.Compose([
-         transforms.Resize([150,150]),
-         transforms.CenterCrop(150),
-
-         transforms.ToTensor(),
-         transforms.Normalize(mean = [0.5,0.5,0.5],std = [0.5,0.5,0.5])
-
-])
-DIR_NAME = str(sys.argv[1])
 import glob, os
 from PIL import Image
-os.chdir(DIR_NAME )
-roots = glob.glob('*.jpg')
+print('load model...')
+model = torch.load('resner10111', map_location=torch.device('cpu'))  # load model
+
+transform_val = transforms.Compose([  # transform needed size
+    transforms.Resize([150, 150]),
+    transforms.CenterCrop(150),
+
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+
+])
+
+DIR_NAME = str(sys.argv[1])  # get target file path
+
+
+os.chdir(DIR_NAME)
+roots = glob.glob('*.jpg')  # create massive with pathes
 
 answer = {}
 for name in roots:
-  tens = transform_val(Image.open(name)).reshape([1,3,150,150])
-  outputs= model(tens)
-  _,label =torch.max(outputs.data, 1)
-  answer[name] = 'male' if label.item()==0 else 'female'
+    tens = transform_val(Image.open(name)).reshape([1, 3, 150, 150])  # transform photo
+    outputs = model(tens)  # model classifier
+    _, label = torch.max(outputs.data, 1)  # get label
+    answer[name] = 'male' if label.item() == 0 else 'female'  # 1 - female, 0- male
 
-
-with open("process_results.json", "w") as fp:
-    json.dump(answer,fp)
+with open("process_results.json", "w") as fp:  # save json file
+    json.dump(answer, fp)
     print('result was saved in {}process_results.json'.format(DIR_NAME))
-
